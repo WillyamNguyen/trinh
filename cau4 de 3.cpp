18996f323381f45cf47f8044731eb3ad39babb50
WillyamNguyen <46101632+WillyamNguyen@users.noreply.github.com>
2018-12-28 09:58:32 +0700

    thu 6

---
+++ cau4 de 3.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int n;
+fstream fi;
+void DocFile(int a[10][10]){
+	fi.open("Bieu Do.txt",ios :: in);
+	fi>>n;
+	for(int i = 0; i< n; i++){
+		for(int j = 0; j < n; j++){
+			fi>>a[i][j];
+		}
+	}
+	fi.close();
+}
+void TaoMoi(int a[10][10], int &n){
+	cout << "Nhap n"; 
+	cin >> n;
+	fi.open("Bieu Do.txt",ios :: out);
+			fi << n;
+		fi >>n;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cout<< "Trong so" << i << "den" << j;
+				cin >> a[i][j];
+				fi << a[i][j] << "_";
+			}
+		}
+		fi<<endl;
+		fi.close();
+} 
+void XuatBieuDo(int a[10][10]){
+	for(int i = 0; i < n; i++){
+		for(int j = 0;j < n; j++){
+			cout << a[i][j] << "_";
+		}
+	}
+}
